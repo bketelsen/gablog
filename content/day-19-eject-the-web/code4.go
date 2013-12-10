9fe33b295be89fe007b0f91b3f719d71966a58b9
mattn <mattn.jp@gmail.com>
2013-12-10 11:10:20 +0900

    Fixes nuance

---
+++ content/day-19-eject-the-web/code4.go
@@ -0,0 +1,50 @@
+package main
+
+import (
+	"github.com/nsf/termbox-go"
+	"time"
+)
+
+func main() {
+	event := make(chan termbox.Event)
+	go func() {
+		for {
+			event <- termbox.PollEvent()
+		}
+	}()
+
+	dirty := false
+	timer := time.AfterFunc(0, func() {
+		if dirty {
+			redraw_full()
+		} else {
+			redraw_part()
+		}
+	})
+
+	for {
+		select {
+		case ev := <-event:
+			// handle terminal event
+			switch ev.Type {
+			case termbox.EventKey:
+				// handle key event
+				switch ev.Key {
+				case termbox.KeyEnter:
+					// update internal condition
+					update_condition(ev)
+					dirty = false
+					// redraw immediately
+					timer.Reset(1 * time.Microsecond)
+				default:
+					// update candidate
+					update_candidate(ev)
+					dirty = true
+					// redrwa in later
+					timer.Reset(200 * time.Microsecond)
+				}
+			}
+		// handle another events
+		}
+	}
+}
